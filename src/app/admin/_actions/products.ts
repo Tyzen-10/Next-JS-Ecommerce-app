b4dd93c7cfe871a8b14bde1daf310c5f00f692e2
Venkatesan R S <96675735+Tyzen-10@users.noreply.github.com>
2024-09-24 16:17:31 +0530

    Zod Validation onSubmit
    Working zod validation and useFormState working well with server actions.

--- src/app/admin/_actions/products.ts
+++ src/app/admin/_actions/products.ts
@@ -1,5 +1,56 @@
 "use server"
+import prisma from "@/db/client"
+import {z} from "zod"
+import fs from "fs/promises"
+import { redirect } from "next/navigation"
 
-export async function addProduct(formData: FormData){
+const fileSchema = z.instanceof(File, {message: "Required!"})
+const imageSchema = fileSchema.refine(file=> file.size === 0 || file.type.startsWith("image/"))
+//or statement -> if either one is true then don't accept i guess -> check later.
+//file size 0 -> so didnt submit file. 
+//if submitted a file then we check whether it starts with image/
+//above we don't check foe file size. we check later below.
+const addSchema = z.object({
+    name:z.string().min(1),
+    description: z.string().min(1),
+    priceInCents:z.coerce.number().int().min(1),
+    //coerce attempts to convert to a number
+    file: fileSchema.refine(file=> file.size > 0, "Required!"),
+    image:imageSchema.refine(file=> file.size > 0, "Required!"),
+    //file and image don't got the z.___ functions. -> so we need to create own checks like above.
+})
+//actions must be an async function -> why?
+export async function addProduct(prevState: unknown,formData: FormData){
+    const result = addSchema.safeParse(Object.fromEntries(formData.entries()))
+    if(!result.success){
+        //not in proper format. -> send errors back to client from SS.
+        return result.error.formErrors.fieldErrors // dont understand this
+    }
+    //code if in proper format.
+    const data = result.data
+
+    await fs.mkdir("products", { recursive : true })
+    const filePath = `products/${crypto.randomUUID()}-${data.file.name}`
+    //now save it to the file path we designed above.
+    await fs.writeFile(filePath, Buffer.from(await data.file.arrayBuffer()))
+
+    await fs.mkdir("public/products", {recursive: true})
+    const imagePath = `/products/${crypto.randomUUID()}-${data.image.name}`
+    /*why dont't we need to give public/products in above line -> but instead /products -> 
+    becoz WDS said it will assume to get it from here, from the folder which is publically available.*/
+    await fs.writeFile(`public${imagePath}`, Buffer.from(await data.image.arrayBuffer()))
+    /*but after saying that, he prefixes image path with public idk why> */
+    
+    await prisma.product.create({data: {
+        name: data.name,
+        description: data.description,
+        priceInCents: data.priceInCents,
+        //for filePath and imagePath -> first store the file in File system 
+        //then path in DB.
+        filePath,
+        imagePath,
+    }})
     console.log(formData);
+
+    redirect("/admin/products")
 }
--- src/app/admin/products/_components/ProductForm.tsx
+++ src/app/admin/products/_components/ProductForm.tsx
@@ -6,36 +6,53 @@ import { Textarea } from '@/components/ui/textarea'
 import { formatCurrency } from '@/lib/formatters'
 import React, { useState } from 'react'
 import { addProduct } from '../../_actions/products'
+import { useFormState, useFormStatus } from 'react-dom'
 
 const ProductForm = () => {
     const [priceInCents, setPriceInCents] = useState<number>();
     //this specifies that above useState stores a type number value.
+    const [error, action] = useFormState(addProduct, {}) 
+    /* 
+    Learn more on this 
+    Like why is the initial state set to an empty object.
+    */ 
   return (
-    <form action={addProduct} className='space-y-8'> 
+    <form action={action} className='space-y-8'> 
         <div className='space-y-2'>
             <Label htmlFor='name'>Name</Label>
             <Input type='text' id='name' name='name' required></Input>
+            {error.name && <div className='text-destructive'>{error.name}</div>}
         </div>
         <div className='space-y-2'>
             <Label htmlFor='priceInCents'>Price in Cents</Label>
             <Input type='number' id='priceInCents' name='priceInCents' required value={priceInCents} onChange={e=>setPriceInCents(Number(e.target.value) || undefined)}></Input>
             <div className='text-muted-foreground'>{formatCurrency((priceInCents || 0) / 100)}</div>
+            {error.priceInCents && <div className='text-destructive'>{error.priceInCents}</div>}
         </div>
         <div className='space-y-2'>
             <Label htmlFor='description'>Description</Label>
             <Textarea id='description' name='description'></Textarea>
+            {error.description && <div className='text-destructive'>{error.description}</div>}
         </div>
         <div className='space-y-2'>
             <Label htmlFor='file'>File</Label>
             <Input type='file' id='file' name='file' required></Input>
+            {error.file && <div className='text-destructive'>{error.file}</div>}
         </div>
         <div className='space-y-2'>
             <Label htmlFor='image'>Image</Label>
             <Input type='file' id='image' name='image' required></Input>
+            {error.image && <div className='text-destructive'>{error.image}</div>}
         </div>
-        <Button type='submit'>Save</Button>
+        <SubmitButton></SubmitButton>
     </form>
   )
 }
+
+function SubmitButton() {
+    const {pending} = useFormStatus()
+    return <Button type='submit' disabled={pending}>{pending ? "Saving": "Save"}</Button>
+}
+
 
 export default ProductForm
